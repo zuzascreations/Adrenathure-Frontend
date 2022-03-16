d399e1cde3f50595fcf605fc92b3bde7de585aa6
manuelpita <manuel.pita@mundo-r.com>
2022-03-16 17:55:50 +0100

    4º added text to Legal Text

--- src/footer/TerminosYCondiciones.js
+++ src/footer/TerminosYCondiciones.js
@@ -2,39 +2,39 @@ function TerminosYCondiciones() {
 
     return(
       <div>
-        <h1>Términos y condiciones</h1>
+      <h1>Términos y Condiciones</h1>
 ‍
-Estos términos y condiciones regulan el acceso y uso de www.hackaboss.com (en adelante, “HACK A BOSS” o la “página web”).
+Estos términos y condiciones regulan el acceso y uso de www.adrenathure.com (en adelante, “ADRENATHURE” o la “página web”).
 
 
 
 1.- Información legal
-Denominación Social - Titular: HACK A BOSS, S.L.
+Denominación Social - Titular: ADRENATHURE
 
-NIF: B70559190
+NIF: B-XX.XXX.XXX
 
-Domicilio: c/Real 1, entreplanta, CP 15001, A Coruña
+Domicilio: Avda. de García Barbón, 27, CP 36201, Vigo
 
-Email: hello@hackaboss.com
+Email: adrenathure.info@gmail.com
 
-Inscripción: Registro Mercantil de A Coruña; T 3655, F 70, H C-56851, I/A 1ª
+Inscripción: Registro Mercantil de _______; T XXXX, F YY, H P-ZZZZZ, I/A Qª
 
-Actividad: Formación
+Actividad: Viajes
 
-Nombre de Dominio: https://www.hackaboss.com/
+Nombre de Dominio: https://www.adrenathure.com/
 
 Registro de Tratamientos conforme al RGPD: Clientes/Proveedores, Listas de Correo, Empleados, Selección de Personal, Usuarios Web y Formación de Terceros.
 
 ‍
 
 2.- Aceptación
-Mediante el acceso a “hackaboss.com” se presume la aceptación del contenido de estos Términos y Condiciones, junto con la Política de Privacidad y Cookies, y se garantiza el compromiso a no utilizarlas para fines ilegales, ilícitos o contrarios a estos términos o condiciones.
+Mediante el acceso a “adrenathure.com” se presume la aceptación del contenido de estos Términos y Condiciones, junto con la Política de Privacidad y Cookies, y se garantiza el compromiso a no utilizarlas para fines ilegales, ilícitos o contrarios a estos términos o condiciones.
 
-La edad mínima para el uso de “hackaboss.com” se establece en 16 años. En el caso de los menores de edad, serán responsables los padres, tutores o representantes legales.
+La edad mínima para el uso de “adrenathure.com” se establece en 16 años. En el caso de los menores de edad, serán responsables los padres, tutores o representantes legales.
 ‍
 
 3.- Finalidad de la web
-La web de HACK A BOSS tiene la finalidad de informar a clientes y potenciales clientes de sus productos y/o servicios, datos de contacto, ubicación, etc. Además, en su caso, también tendrá la finalidad de divulgar información relacionada con el sector de actividad del Responsable.
+La web de ADRENATHURE tiene la finalidad de informar a clientes y potenciales clientes de sus productos y/o servicios, datos de contacto, ubicación, etc. Además, en su caso, también tendrá la finalidad de divulgar información relacionada con el sector de actividad del Responsable.
 
 4.- Marco Normativo
 La actividad de esta web se encuentra sujeta al marco legal español y europeo, concretamente a las siguientes normas:
@@ -43,62 +43,62 @@ Reglamento General de Protección de Datos (RGPD) (UE) 2016/679, de 27 de abril,
 Ley Orgánica 3/2018, de 5 de diciembre, sobre Protección de Datos Personales y Garantía de los Derechos Digitales (LOPD y GDD), norma de ámbito regional (aplicable a España), y que define y amplía muchos de los conceptos y derechos presentes en el RGPD.
 ‍
 5.- Condiciones de Uso y Responsabilidades
-Toda persona que acceda a este sitio web asume el papel de Usuario, comprometiéndose a la observancia y cumplimiento riguroso de las condiciones aquí dispuestas, así como a cualquiera otra disposición legal que fuera de aplicación. HACK A BOSS no se hará responsable de los daños y perjuicios, propios o a terceros, producidos por el uso inadecuado de este sitio web por parte del Usuario.
+Toda persona que acceda a este sitio web asume el papel de Usuario, comprometiéndose a la observancia y cumplimiento riguroso de las condiciones aquí dispuestas, así como a cualquiera otra disposición legal que fuera de aplicación. ADRENATHURE no se hará responsable de los daños y perjuicios, propios o a terceros, producidos por el uso inadecuado de este sitio web por parte del Usuario.
 
 El contenido de esta web, por tanto, no puede ser considerado, en ningún caso, sustitutivo de asesoramiento legal.
 
-HACK A BOSS se reserva el derecho a modificar cualquier tipo de información que pudiera aparecer en la web, en cualquier momento y sin previo aviso, sin que exista obligación de preavisar o poner en conocimiento de los Usuarios dichas obligaciones, entendiéndose como suficiente la publicación en el presente sitio web.
+ADRENATHURE se reserva el derecho a modificar cualquier tipo de información que pudiera aparecer en la web, en cualquier momento y sin previo aviso, sin que exista obligación de preavisar o poner en conocimiento de los Usuarios dichas obligaciones, entendiéndose como suficiente la publicación en el presente sitio web.
 
-Este sitio web ha sido revisado y probado para que funcione correctamente de manera ininterrumpida. No obstante, HACK A BOSS no descarta la posibilidad de que existan ciertos errores de programación, falta de disponibilidad puntual (por ejemplo, caídas del servidor, o mantenimiento del mismo) o que acontezcan causas de fuerza mayor, catástrofes naturales, huelgas, o circunstancias semejantes que hagan imposible el acceso temporal a la página web. De igual forma, HACK A BOSS no puede garantizar el funcionamiento ininterrumpido o totalmente libre de errores de esta web, ni se responsabiliza de los virus que tengan su origen en una transmisión telemática infiltrados por terceros generados con la finalidad de obtener resultados negativos para un sistema informático.
+Este sitio web ha sido revisado y probado para que funcione correctamente de manera ininterrumpida. No obstante, ADRENATHURE no descarta la posibilidad de que existan ciertos errores de programación, falta de disponibilidad puntual (por ejemplo, caídas del servidor, o mantenimiento del mismo) o que acontezcan causas de fuerza mayor, catástrofes naturales, huelgas, o circunstancias semejantes que hagan imposible el acceso temporal a la página web. De igual forma, ADRENATHURE no puede garantizar el funcionamiento ininterrumpido o totalmente libre de errores de esta web, ni se responsabiliza de los virus que tengan su origen en una transmisión telemática infiltrados por terceros generados con la finalidad de obtener resultados negativos para un sistema informático.
 
-El Usuario se compromete a no utilizar esta web ni, si es el caso, los servicios o productos ofrecidos en la misma, para la realización de actividades contrarias a la ley, al orden público o a estas condiciones de uso. Por tanto, HACK A BOSS no se hace responsable de la información y contenidos almacenados, a título enunciativo, pero no limitativo, en foros, chats, generadores de blogs, comentarios, redes sociales o cualquier otro medio que permita a terceros publicar contenidos. No obstante, y en cumplimiento de lo dispuesto en el art. 11 y 16 de la LSSI-CE, HACK A BOSS se pone a disposición de todos los Usuarios, autoridades y fuerzas de seguridad, colaborando de forma activa en la retirada o en su caso bloqueo de todos aquellos contenidos que pudieran afectar o contravenir la legislación nacional, o internacional, derechos de terceros, o la moral y el orden público. En caso de que un Usuario considere que existe en la web algún contenido que pudiera ser susceptible de esta clasificación, se ruega lo notifique de forma inmediata a nuestro personal.
+El Usuario se compromete a no utilizar esta web ni, si es el caso, los servicios o productos ofrecidos en la misma, para la realización de actividades contrarias a la ley, al orden público o a estas condiciones de uso. Por tanto, ADRENATHURE no se hace responsable de la información y contenidos almacenados, a título enunciativo, pero no limitativo, en foros, chats, generadores de blogs, comentarios, redes sociales o cualquier otro medio que permita a terceros publicar contenidos. No obstante, y en cumplimiento de lo dispuesto en el art. 11 y 16 de la LSSI-CE, ADRENATHURE se pone a disposición de todos los Usuarios, autoridades y fuerzas de seguridad, colaborando de forma activa en la retirada o en su caso bloqueo de todos aquellos contenidos que pudieran afectar o contravenir la legislación nacional, o internacional, derechos de terceros, o la moral y el orden público. En caso de que un Usuario considere que existe en la web algún contenido que pudiera ser susceptible de esta clasificación, se ruega lo notifique de forma inmediata a nuestro personal.
 
-HACK A BOSS se reserva el derecho a denegar o retirar el acceso a la web sin necesidad de advertencia previa, a instancia propia o de un tercero, a aquellos Usuarios que incumplan nuestras Condiciones de Uso.
+ADRENATHURE se reserva el derecho a denegar o retirar el acceso a la web sin necesidad de advertencia previa, a instancia propia o de un tercero, a aquellos Usuarios que incumplan nuestras Condiciones de Uso.
 
 ‍
 
 6.- Propiedad intelectual
-El sitio web, incluyendo a título enunciativo, pero no limitativo su programación, edición, compilación y demás elementos necesarios para su funcionamiento, los diseños, logotipos, texto y/o gráficos son propiedad de HACK A BOSS o, en su caso, dispone de licencia o autorización expresa por parte de los autores.
+El sitio web, incluyendo a título enunciativo, pero no limitativo su programación, edición, compilación y demás elementos necesarios para su funcionamiento, los diseños, logotipos, texto y/o gráficos son propiedad de ADRENATHURE o, en su caso, dispone de licencia o autorización expresa por parte de los autores.
 
-Todos los contenidos del sitio web se encuentran debidamente protegidos por la normativa de propiedad intelectual e industrial, así como inscritos en los registros públicos correspondientes, y no se permite la reproducción y/o publicación, total o parcial, del sitio web, ni su tratamiento informático, su distribución, difusión, modificación o transformación, sin el permiso previo y por escrito del mismo. HACK A BOSS velará por el cumplimiento de las anteriores condiciones, así como por la debida utilización de los contenidos presentados en sus páginas web, ejercitando todas las acciones civiles y penales que le correspondan en el caso de infracción o incumplimiento de estos derechos por parte del Usuario.
+Todos los contenidos del sitio web se encuentran debidamente protegidos por la normativa de propiedad intelectual e industrial, así como inscritos en los registros públicos correspondientes, y no se permite la reproducción y/o publicación, total o parcial, del sitio web, ni su tratamiento informático, su distribución, difusión, modificación o transformación, sin el permiso previo y por escrito del mismo. ADRENATHURE velará por el cumplimiento de las anteriores condiciones, así como por la debida utilización de los contenidos presentados en sus páginas web, ejercitando todas las acciones civiles y penales que le correspondan en el caso de infracción o incumplimiento de estos derechos por parte del Usuario.
 
-Los diseños, logotipos, texto y/o gráficos ajenos a HACK A BOSS y que pudieran aparecer en el sitio web, pertenecen a sus respectivos propietarios, siendo ellos mismos responsables de cualquier posible controversia que pudiera suscitarse respecto a los mismos. En todo caso, HACK A BOSS cuenta con la autorización expresa y previa por parte de los mismos. HACK A BOSS reconoce a favor de sus titulares los correspondientes derechos de propiedad industrial e intelectual, no implicando su sola mención o aparición en el sitio web la existencia de derechos o responsabilidad alguna del Responsable sobre los mismos, como tampoco respaldo, patrocinio o recomendación por parte del mismo.
+Los diseños, logotipos, texto y/o gráficos ajenos a ADRENATHURE y que pudieran aparecer en el sitio web, pertenecen a sus respectivos propietarios, siendo ellos mismos responsables de cualquier posible controversia que pudiera suscitarse respecto a los mismos. En todo caso, ADRENATHURE cuenta con la autorización expresa y previa por parte de los mismos. ADRENATHURE reconoce a favor de sus titulares los correspondientes derechos de propiedad industrial e intelectual, no implicando su sola mención o aparición en el sitio web la existencia de derechos o responsabilidad alguna del Responsable sobre los mismos, como tampoco respaldo, patrocinio o recomendación por parte del mismo.
 
 ‍
 
 7.- Enlaces de terceros
-HACK A BOSS puede poner a disposición del Usuario enlaces u otros elementos que permitan el acceso hacia otros sitios web pertenecientes a terceros. No comercializamos los productos y/o servicios de dichas páginas enlazadas, ni asumimos ningún tipo de responsabilidad sobre las mismas, ni sobre la información contenida en ellas, ni su veracidad o licitud, ni de cualesquiera efectos que pudieran derivarse.
+ADRENATHURE puede poner a disposición del Usuario enlaces u otros elementos que permitan el acceso hacia otros sitios web pertenecientes a terceros. No comercializamos los productos y/o servicios de dichas páginas enlazadas, ni asumimos ningún tipo de responsabilidad sobre las mismas, ni sobre la información contenida en ellas, ni su veracidad o licitud, ni de cualesquiera efectos que pudieran derivarse.
 
-En todo caso, HACK A BOSS manifiesta que procederá a la retirada inmediata de cualquier contenido que pudiera contravenir la legislación nacional o internacional, la moral o el orden público, procediendo a la retirada inmediata de la redirección a dicho sitio web, poniendo en conocimiento de las autoridades competentes el contenido en cuestión.
+En todo caso, ADRENATHURE manifiesta que procederá a la retirada inmediata de cualquier contenido que pudiera contravenir la legislación nacional o internacional, la moral o el orden público, procediendo a la retirada inmediata de la redirección a dicho sitio web, poniendo en conocimiento de las autoridades competentes el contenido en cuestión.
 
 ‍
 
 8.- Ley aplicable y Jurisdicción
 El idioma de redacción e interpretación de este aviso legal es el español.
 
-La relación entre HACK A BOSS y Usuario se rige en todos y cada uno de sus extremos por la ley española, a la que se someten expresamente ambas partes ante cualquier controversia o cuestión relacionada con el presente sitio web o las actividades en él desarrolladas.
+La relación entre ADRENATHURE y Usuario se rige en todos y cada uno de sus extremos por la ley española, a la que se someten expresamente ambas partes ante cualquier controversia o cuestión relacionada con el presente sitio web o las actividades en él desarrolladas.
 
-Por su parte, en el caso de que el usuario tenga su domicilio fuera de España, HACK A BOSS y Usuario se someten, con renuncia expresa a cualquier otro fuero, a los Juzgados y Tribunales competentes de España.
+Por su parte, en el caso de que el usuario tenga su domicilio fuera de España, ADRENATHURE y Usuario se someten, con renuncia expresa a cualquier otro fuero, a los Juzgados y Tribunales competentes de España.
 
 ‍
 
 9.- Seguridad y Limitación de Responsabilidad
-HACK A BOSS adoptará las medidas necesarias para asegurar la integridad del servicio disponible, y de los datos e información recogidos, pero no será responsable de lo que ocurra más allá de su control.
+ADRENATHURE adoptará las medidas necesarias para asegurar la integridad del servicio disponible, y de los datos e información recogidos, pero no será responsable de lo que ocurra más allá de su control.
 
-En caso de que el usuario sea redirigido a otras páginas webs o plataformas de terceros, HACK A BOSS no se hará responsable de lo que ocurra a través de la navegación en las mismas.
+En caso de que el usuario sea redirigido a otras páginas webs o plataformas de terceros, ADRENATHURE no se hará responsable de lo que ocurra a través de la navegación en las mismas.
 
 Esta página web está disponible en español, de forma que el titular no se hace responsable en caso de que el usuario no comprenda dicho idioma, ni de los actos que puedan derivar de su falta de comprensión.
 
-En la medida en que lo permita la ley, HACK A BOSS no responde del uso incorrecto o imprudente que realicen los usuarios de “hackaboss.com”.
+En la medida en que lo permita la ley, ADRENATHURE no responde del uso incorrecto o imprudente que realicen los usuarios de “adrenathure.com”.
 
-HACK A BOSS no será responsable de incumplimientos o deficiencias que tengan causa de fuerza mayor o que estén fuera de su control razonable.
+ADRENATHURE no será responsable de incumplimientos o deficiencias que tengan causa de fuerza mayor o que estén fuera de su control razonable.
 
 ‍
 
 
 
 10.- Modificaciones y Nulidad
-HACK A BOSS se reserva el derecho a actualizar y modificar estos textos legales, por lo que recomienda leer los mismos cada vez que se acceda o use la web.En caso de que alguna cláusula de estos Términos y Condiciones, Política de Privacidad o de Cookies, sea declarada total o parcialmente nula o ineficaz, dicha nulidad o ineficacia sólo afectará a la misma, subsistiendo las demás condiciones y teniéndose la disposición afectada por no puesta.
+ADRENATHURE se reserva el derecho a actualizar y modificar estos textos legales, por lo que recomienda leer los mismos cada vez que se acceda o use la web.En caso de que alguna cláusula de estos Términos y Condiciones, Política de Privacidad o de Cookies, sea declarada total o parcialmente nula o ineficaz, dicha nulidad o ineficacia sólo afectará a la misma, subsistiendo las demás condiciones y teniéndose la disposición afectada por no puesta.
 
 ‍
 
@@ -108,7 +108,7 @@ Estos Términos y Condiciones, y las Políticas de Privacidad y de Cookies, se i
 ‍
 
 12.- Contacto
-En caso de duda, sugerencia o aclaración, el usuario se puede poner en contacto con HACK A BOSS a través de la siguiente dirección de correo electrónico: hello@hackaboss.com.
+En caso de duda, sugerencia o aclaración, el usuario se puede poner en contacto con ADRENATHURE a través de la siguiente dirección de correo electrónico: adrenathure.info@gmail.com.
 
 </div>
     )
